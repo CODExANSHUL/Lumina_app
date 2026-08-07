4c1515752f62fbf1b29c5d229757895295b011ba
Anshul <anshulrokadeofficial@gmail.com>
2026-08-07 17:46:34 +0530

    URL issue solved

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,7 +36,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://lumina-app-cg5b.onrender.com"],
+        "https://lumina-app-cg5b.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
