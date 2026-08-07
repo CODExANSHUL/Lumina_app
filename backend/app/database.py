04b57cc5d550d519d6e671bb144400501c292ea1
Anshul <anshulrokadeofficial@gmail.com>
2026-08-07 18:10:10 +0530

    Cors issue solved

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,66 +1,26 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# # 1. Define your URL connection string
-# # db_url = "mysql+pymysql://root:your_password@localhost:3306/my_database"
-
-# # 2. Create the engine
-# # engine = create_engine(db_url)
-
-# # 3. Test and use the connection
-# # with engine.connect() as connection:
-#     # result = connection.execute("SELECT VERSION();")
-#     # print(f"Database version: {result.fetchone()[0]}")
-
-
-# db_url = "mysql://project:Anshul22%40@localhost:3306/project"
-# engine = create_engine(db_url)
-# session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 from sqlalchemy import create_engine
-
-from sqlalchemy.orm import declarative_base
-
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.config import settings
 
-
 engine = create_engine(
-
     settings.DATABASE_URL,
-
     pool_pre_ping=True,
-
     future=True,
-
-    connect_args={
-        "prepare_threshold": None
-    }
-
 )
 
 SessionLocal = sessionmaker(
-
     autocommit=False,
-
     autoflush=False,
-
-    bind=engine
-
+    bind=engine,
 )
 
 Base = declarative_base()
 
 
 def get_db():
-
     db = SessionLocal()
-
     try:
-
         yield db
-
     finally:
-
         db.close()
